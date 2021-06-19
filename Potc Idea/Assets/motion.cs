5d0f31c4d0657acea90a7a6cb42e9776042e8095
frank06n <dev.code38@gmail.com>
2021-06-19 10:57:29 +0530

    Started working

---
+++ Potc Idea/Assets/motion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motion : MonoBehaviour
+{
+    Rigidbody shipRigidbody;
+    public float shipForce;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        shipRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            shipRigidbody.AddForce(transform.forward * shipForce * Time.fixedDeltaTime);
+        }
+    }
+}
